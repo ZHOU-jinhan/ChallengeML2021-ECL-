58873fd7e8de9087f1421c53cab06353fb75c84d
ZHOU Jinhan <55496754+ZHOU-jinhan@users.noreply.github.com>
2021-02-16 22:17:14 +0100

    Relabel of Test_y

--- file_translator.py
+++ file_translator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 Y_train=pd.read_csv(r"y_train.csv", header=0, index_col=0)
-col=Y_train.columns;ind=Y_train.index[-1]
+col=Y_train.columns;ind=Y_train.index[-1]+1
 for f in os.listdir(r"results/test_y"):
     Y = pd.DataFrame(pd.read_csv(r"results/test_y/"+f, header=0, index_col=0).values.astype('float32'),columns=Y_train.columns)
     Y.rename(index={i:i+ind for i in Y.index},inplace=True)
