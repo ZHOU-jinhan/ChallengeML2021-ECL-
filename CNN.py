e1cdfa899fdf3c502a244a63b11280094c4b9c00
ZHOU Jinhan <55496754+ZHOU-jinhan@users.noreply.github.com>
2021-03-10 11:17:25 +0100

    Add files via upload

--- CNN.py
+++ CNN.py
@@ -7,6 +7,7 @@ from pandas import concat
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
+import tensorflow as tf
 from tensorflow.keras.models import Sequential,load_model
 from tensorflow.keras.layers import Reshape,BatchNormalization,LeakyReLU,Dense,LSTM,Dropout,Conv2D,MaxPooling2D
 import h5py
@@ -21,34 +22,37 @@ class myCNN:
         # load dataset
         h5_file=h5py.File(path_x,"r");X_data=(np.array(h5_file["data"][:, 2:])).astype('float32')
         X_train = X_data.reshape(X_data.shape[0],8,int(X_data.shape[1]/800),100)
-        X_train = X_train.transpose(0,2,1,3)
+        X_train = X_train.transpose(0,2,3,1);X=X_train.reshape(-1,8);
+        self.mean = np.mean(X,axis=0);self.aplt = np.max(X,axis=0)-np.min(X,axis=0)
+        X_train = (X_train-self.mean)/self.aplt
+        X_train = X_train.transpose(0,1,3,2)
         Y_train = read_csv(path_y, header=0, index_col=0).values.astype('float32')
         # split into train and test sets
         n_train_hours = int(X_train.shape[0]*train_split)
         # split into input and outputs
         train_X, train_y = X_train[:n_train_hours,:,:,:], Y_train[:n_train_hours, :]
         if train_split!=1:
-            test_X, test_y = X_train[n_train_hours:,:,:,:], Y_train[n_train_hours:, :]
+            test_X, test_y = X_train[n_train_hours:, :,:,:], Y_train[n_train_hours:, :]
         print(train_X.shape,train_y.shape)
         # design network
         if self.model==None:
             self.model = Sequential()
             # Convolution layer
-            self.model.add(Conv2D(16,[3,3], input_shape=(train_X.shape[-3],train_X.shape[-2], train_X.shape[-1]), padding='same', kernel_initializer='he_normal'))  # (None, ls, 38, 32)
-            self.model.add(MaxPooling2D([1,2],padding="same"))
-            self.model.add(BatchNormalization(momentum=0.8))
-            self.model.add(LeakyReLU(alpha=0.2))
+            self.model.add(Conv2D(32,[3,3], padding='same', kernel_initializer='he_normal',input_shape=(train_X.shape[-3],train_X.shape[-2], train_X.shape[-1]))) 
+            self.model.add(BatchNormalization(momentum=0.75))
+            self.model.add(MaxPooling2D([2,2],padding="same"))
+            self.model.add(Conv2D(64,[3,3], padding='same', kernel_initializer='he_normal')) 
+            self.model.add(BatchNormalization(momentum=0.75))
+            self.model.add(MaxPooling2D([2,2],padding="same"))
+            self.model.add(Conv2D(64,[3,3], padding='same', kernel_initializer='he_normal')) 
+            self.model.add(BatchNormalization(momentum=0.75))
+            self.model.add(MaxPooling2D([2,2],padding="same"))
+            self.model.add(Reshape(target_shape=((-1,))))
             self.model.add(Dropout(0.25))
-            self.model.add(Conv2D(32, [2,5], padding='same', kernel_initializer='he_normal'))  # (None, ls, 38, 64)
-            self.model.add(LeakyReLU(alpha=0.2))
+            self.model.add(Dense(256))
             self.model.add(Dropout(0.25))
-            self.model.add(Conv2D(32, [5,2], padding='same', kernel_initializer='he_normal'))  # (None, ls/2, 19, 64)
-            self.model.add(LeakyReLU(alpha=0.2))
-            self.model.add(Dropout(0.25))
-            self.model.add(Reshape(target_shape=((90,-1))))
-            # CNN to RNN
-            self.model.add(Dense(1,activation="sigmoid",kernel_initializer='he_normal'))
-            self.model.compile(loss='mae', optimizer='adam')
+            self.model.add(Dense(90,activation="softmax",kernel_initializer='he_normal'))
+            self.model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=tf.keras.optimizers.Adam(lr=0.2e-3))
             self.model.summary()
         # fit network
         if train_split!=1:
@@ -84,16 +88,17 @@ class myCNN:
         if plot:
             pyplot.show()
 
-    def predict(self,path_x='X_test.h5'):
+    def predict(self,path_x='X_train.h5'):
         # load dataset
         h5_file = h5py.File(path_x,"r")
         test_X = (np.array(h5_file["data"][:, 2:])).astype('float32')
         test_X = test_X.reshape(test_X.shape[0],8,int(test_X.shape[1]/800),100)
-        test_X = test_X.transpose(0,2,1,3)
-        test_y = self.model.predict(test_X)
-        test_y = test_y.reshape(test_y.shape[0],test_y.shape[1])
-        pd.DataFrame(test_y).to_csv("results/y_hat/CNN_y_hat.csv")
-        pd.DataFrame((test_y>self.d_threshold).astype("int32")).to_csv("results/test_y/CNN_y.csv")
+        test_X = test_X.transpose(0,2,3,1)
+        test_X = (test_X-self.mean)/self.aplt
+        test_X = test_X.transpose(0,1,3,2)
+        pd.DataFrame((test_y>self.d_threshold).astype("int32")).to_csv("train/CNN_y.csv")
+##        pd.DataFrame(test_y).to_csv("results/y_hat/CNN_y_hat.csv")
+##        pd.DataFrame((test_y>self.d_threshold).astype("int32")).to_csv("results/test_y/CNN_y.csv")
 
     def save(self):
         """
@@ -112,5 +117,5 @@ class myCNN:
 
 if __name__=="__main__":
     cnn=myCNN()
-    cnn.koad()
+    cnn.train()
     cnn.predict()
